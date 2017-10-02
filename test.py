e8b442e4e89b042043f5f30233452aa397f48a71
geofrocker <geofrocker2@gmail.com>
2017-10-02 14:40:47 +0300

    Feature/Add test.py file

--- test.py
+++ test.py
@@ -10,6 +10,36 @@ def test_home_end_point():
 def test_add_Recipe_end_point():
     """Test add_Recipe_end_point"""
     client = app.test_client()
-    rsp = client.get('/recipe')
+    rsp = client.post('/recipe/1')
+    assert rsp.status == '200 OK'
+
+def test_edit_Recipe_end_point():
+    """Test edit_Recipe_end_point"""
+    client = app.test_client()
+    rsp = client.put('/recipe/1')
+    assert rsp.status == '200 OK'
+
+def test_delete_Recipe_end_point():
+    """Test edit_Recipe_end_point"""
+    client = app.test_client()
+    rsp = client.delete('/recipe/1')
+    assert rsp.status == '200 OK'
+
+def test_delete_Recipe_end_point():
+    """Test edit_Recipe_end_point"""
+    client = app.test_client()
+    rsp = client.delete('/recipe/1')
+    assert rsp.status == '200 OK'
+
+def test_Register_user_end_point():
+    """Test Register_user_end_point"""
+    client = app.test_client()
+    rsp = client.post('/register')
+    assert rsp.status == '200 OK'
+
+def test_Authentication_user_end_point():
+    """Test Authenticate_user_end_point"""
+    client = app.test_client()
+    rsp = client.get('/login')
     assert rsp.status == '200 OK'
 
