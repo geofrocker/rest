9c8fa31f0118751ea2eb2509e3c271d312a48dac
geofrocker <geofrocker2@gmail.com>
2017-10-04 19:58:06 +0300

    Refactor/cleaning code and making sure that it is DRY

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:elizei2@127.0.0.1:5432/rest_api'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = os.urandom(24)
+SECRET_KEY = "yeah"
     
--- views.py
+++ views.py
@@ -1,35 +1,14 @@
 """/rest api views.py"""
 import uuid
 from datetime import datetime, timedelta
-from functools import wraps
 from flask_restful import reqparse, Resource, marshal
-import jwt
+
 from flask import jsonify, make_response, request
 from werkzeug.security import check_password_hash, generate_password_hash
 from serializer import recipe_serializer, user_serializer
-from app import app
-from app import api
-from models import *
-
-
-def token_required(f):
-    """
-    Ensure only logged in users access the system
-    """
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            return jsonify({'Message': 'You have no access token'}), 401
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            current_user = User.query.filter_by(id=data['id']).first()
-        except:
-            return jsonify({'message' : 'Invalid Token'}), 401
-        return f(current_user, *args, **kwargs)
-    return decorated
+from app import app, api, db
+from models import User, Recipe
+from decorators import *
 
 class Recipes(Resource):
     def get(self):
@@ -42,8 +21,8 @@ class Recipes(Resource):
             return {"Recipe_list": recipe_list}, 200
         else:
             return {'message': 'No recipes found'}, 404
-    
-    def post(self):
+    @token_required
+    def post(current_user, self):
         """
         Add a recipe to the database
         """
@@ -58,7 +37,8 @@ class Recipes(Resource):
 
 api.add_resource(Recipes, '/')
 class RecipeItem(Resource):
-    def get(self,id):
+    @token_required
+    def get(current_user, self,id):
         """get on one recipe"""
         recipe = Recipe.query.filter_by(id=id).first()
         if not recipe:
@@ -68,9 +48,8 @@ class RecipeItem(Resource):
             return {"Recipe_list": recipe_list}, 200
         else:
             return {'message': 'Recipes Not found'}, 404
-
-    @app.route('/recipe/<id>', methods=['PUT'])
-    def put(self, id):
+    @token_required
+    def put(current_user, self, id):
         """Edit a recipe by id"""
         data = request.get_json()
         recipe = Recipe.query.filter_by(id=id).first()
@@ -82,7 +61,8 @@ class RecipeItem(Resource):
         db.session.commit()
         return {'message':'User Edited successfully'}
 
-    def delete(self, id):
+    @token_required
+    def delete(current_user, self, id):
         """Delete recipe by id"""
         recipe = Recipe.query.filter_by(id=id).first()
         if not recipe:
@@ -118,18 +98,19 @@ class AuthLogin(Resource):
             return make_response('Could nott verify user', 401, {'WWW-Authenticate' : 'Basic Realm="Login Required"'})
 
         if check_password_hash(user.password, auth.password):
-            token = jwt.encode({'id' : user.id, 'exp' : datetime.utcnow() + timedelta(seconds=30)}, app.config['SECRET_KEY'])
+            token = jwt.encode({'id' : user.id, 'exp' : datetime.utcnow() + timedelta(minutes=30)}, app.config['SECRET_KEY'])
             return jsonify({'token' : token.decode('UTF-8')})
         return make_response('Could nottt verify user', 401, {'WWW-Authenticate' : 'Basic Realm="Login Required"'})
 
 api.add_resource(AuthLogin, '/auth/login')
 
 class Users(Resource):
-    def get(self):
+    @token_required
+    def get(current_user, self):
         """
         get a list of all users
         """
-        users = User.query.paginate(per_page=4, page=page_num, error_out=True)
+        users = User.query.all()
         if users:
             user_list = marshal(users, user_serializer)
             return {"user_list": user_list}, 200
@@ -138,8 +119,9 @@ class Users(Resource):
 
 api.add_resource(Users, '/users')
 
-class User(Resource):
-    def get(self,id):
+class OneUser(Resource):
+    @token_required
+    def get(current_user, self, id):
         """
         Get_one_user
         """
@@ -150,8 +132,8 @@ class User(Resource):
         else:
             return {'message': 'User not found'}, 404
     
-    
-    def delete(self, id):
+    @token_required
+    def delete(current_user, self, id):
         """
         Delete_one_user
         """
@@ -162,4 +144,4 @@ class User(Resource):
         db.session.commit()
         return {'Message':'User deleted'}
 
-api.add_resource(User, '/users/<id>')
+api.add_resource(OneUser, '/users/<id>')
