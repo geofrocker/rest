881f984c61d3dc835a4a7b2c50538257fc6b4a78
geofrocker <geofrocker2@gmail.com>
2017-10-06 02:32:35 +0300

    Refactor/remove unnnecesary files

--- routes.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-from app import api
-
-api.add_resource(RecipeItem, '/<id>')
-api.add_resource(AuthRegister, '/auth/register')
-api.add_resource(AuthLogin, '/auth/login')
-api.add_resource(Users, '/users')
-api.add_resource(OneUser, '/users/<id>')
