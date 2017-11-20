89c9e94aeaa676521b332886acf17b20d901c7e9
geofrocker <geofrocker2@gmail.com>
2017-11-20 18:27:53 +0300

    Refactor/Update manage.py file

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import db, app
+from recipe import db, app
 from models import *
 
 migrate = Migrate(app, db)
