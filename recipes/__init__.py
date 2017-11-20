d724230e120966e0c8b069d18c4316d752154cc9
geofrocker <geofrocker2@gmail.com>
2017-11-20 23:24:09 +0300

    Update/Set environment variables in travis

--- recipes/__init__.py
+++ recipes/__init__.py
@@ -9,11 +9,11 @@ from decouple import config
 app = Flask(__name__)
 CORS(app)
 #app.config.from_pyfile('config.py')
-app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 #app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
 app.config['DEBUG'] = config('DEBUG', default=False, cast=bool)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 db = SQLAlchemy(app)
 api = Api(app)
