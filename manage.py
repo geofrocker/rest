792727d208b31d39115989f5347741a125e9354f
geofrocker <geofrocker2@gmail.com>
2017-12-11 23:40:02 +0300

    Refactor/Add final pep8 corrections

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from recipes import db, app
-
-migrate = Migrate(app, db)
+MIGRATION_DIR = os.path.join('app', 'migrations')
+migrate = Migrate(app, db, directory=MIGRATION_DIR)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
